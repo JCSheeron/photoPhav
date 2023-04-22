c455674e97436cd5b1c216f562a95b6c5311c5d3
JCSheeron <jcs@san.rr.com>
2023-04-22 13:23:26 -0700

    Source and dest directory working. Now have list of source files, taking into account glob filtering, regex filtering, recursive dir search option, and ignore case option

--- photoPhav.py
+++ photoPhav.py
@@ -17,9 +17,11 @@
 # Varialbe Naming Conventions:
 # Class Names: normally use CapWords Conventions
 # Type Variable Names: CapWords convention, or single cap letter. Prefer short names.
-# Exception Names: CapWords or follow class naming convention. Suffix should be Error, etc.
+# Exception Names: CapWords or follow class naming convention.
+#   Suffix should be 'Error', 'Err', 'Warn', etc.
 # Function Names: lowercase_with_underscores. or mixedCase if already prevailing.
-# Variable Names: Follow naming of functions. lowercase_with_underscores or mixedCase if already prevailing.
+# Variable Names: Follow naming of functions. lowercase_with_underscores or
+#   mixedCase if already prevailing.
 # Function and Method Arguments:
 #   'self' as first argument for instance methods.
 #   'cls' as first argument to class methods.
@@ -41,13 +43,16 @@ import argparse
 # imports
 #
 # Standard library and system imports
-# import sys
-import re  # regular expressions
+import sys
+
 # path and file processing
 from pathlib import Path
+# file name matching and regular expressions
+import fnmatch
+import re
 
 # Third party and library imports
-
+from bpsPrettyPrint import listPrettyPrint1Col
 
 # Local application and user library imports
 
@@ -72,24 +77,24 @@ def main():
     MPEG2, MPEG4, AVI, FLV, SWF, ASF, POSTSCRIPT, P2, SONYHDV, AVCHD, UCF, WAV,
     XDCAM, XDCAMEX.
 
-    The star rating threshold can be specified with the -sr, --star-rating <rating>
+    The star rating threshold can be specified with the -sr, --star_rating <rating>
     option, where rating is 1-5. Values at or above the indicated value will be
     included. Default: 1
 
-    The star rating will be ignored if the -is, --ignore-star option is given.
+    The star rating will be ignored if the -is, --ignore_star option is given.
 
-    The color rating threshold can be specified with the -cr, --color-rating <rating>
+    The color rating threshold can be specified with the -cr, --color_rating <rating>
     option where rating is ???. Values at or above the indicated value will be
     included. **Default value??** NOTE: Not yet supported.
 
-    The color rating will be ignored if the -ic, --ignore-color option is given.
+    The color rating will be ignored if the -ic, --ignore_color option is given.
     Default: true (ignore color rating by default)
 
-    A source directory can be provided with the -sd, --source-dir <path> option
+    A source directory can be provided with the -sd, --source_dir <path> option
     to specify the directory to use for the source images. If this option is
     not provided, the working directory will used as the source directory.
 
-    A destinaiton directory can be provided with the -dd, --dest-dir <path> option
+    A destinaiton directory can be provided with the -dd, --dest_dir <path> option
     to specify the directory to use for the link destination. If this option
     is not provided, a 'favorites' sub directory in the working directory
     will first be created if it does not exist, and will used for the
@@ -99,21 +104,21 @@ def main():
     images are found in sub folders, the same directory structure will be used
     for the destination directory structure.
 
-    The -f, --file-types <type> or <type list> option allows a single file type,
+    The -f, --file_types <type> or <type list> option allows a single file type,
     or space, comma, or semicolon separated list of file types.
     Supported file types are listed above. The option values not case sensitive,
     but otherwise need to match one of the options lsited above.
 
-    The -x, --xmp-priority option will give priority to information in an xmp
+    The -x, --xmp_priority option will give priority to information in an xmp
     sidecar file if one exists. Without this option, priority is given to the
     image file over the xmp file, and the xmp file would only be used if there
     was not rating or color information in the image file. In all cases, in
     order to be considered, a file with the same name as the image file ending
     in 'xmp' (case insensitive) must be found. Mutually exclusive with the
-    -ix/--ignore-xmp option.
+    -ix/--ignore_xmp option.
 
-    The -ix, --ignore-xmp option will ignore xmp sidecar file(s), even if they
-    are present. Mutually exclusive with the -x/--xmp-priority option.
+    The -ix, --ignore_xmp option will ignore xmp sidecar file(s), even if they
+    are present. Mutually exclusive with the -x/--xmp_priority option.
 
     The -g, --globp <pattern> option allows files to be searched using a glob
     pattern. Mutually exclusive with the -e/--regexp option.
@@ -121,7 +126,7 @@ def main():
     The -e, --regexp <pattern> option allows files to be searched using a
     regular expression. Mutually exclusive with the -g/--globp option.
 
-    The -i, --ignore-case option ignores file name case when matching using a glob
+    The -i, --ignore_case option ignores file name case when matching using a glob
     pattern or regex patten. This option is ignored if neither the -g or -e,
     patterns are specified.
 
@@ -131,8 +136,7 @@ def main():
     The -q, --quiet option eliminates output messaging, even in the event of
     errors. Mutually exclusive with -v and -w.
 
-    The -w, --show-errors-and-warnings option does exactly that. Mutually
-    exclusive with -v and -q.
+    The -w, --show_ew option does exactly that. Mutually exclusive with -v and -q.
     """
     # Descripiton string will show up in help.
     DESC_STR = main.__doc__
@@ -150,7 +154,7 @@ def main():
     # star rating
     parser.add_argument(
         "-sr",
-        "--star-rating",
+        "--star_rating",
         default=1,
         type=int,
         choices=range(1, 6),  # 1-5
@@ -159,12 +163,12 @@ def main():
 and a link will be created. Default value is 1, so any stared image is a favorite.",
     )
     parser.add_argument(
-        "-is", "--ignore-star", action="store_true", help="Ignore star ratings if set."
+        "-is", "--ignore_star", action="store_true", help="Ignore star ratings if set."
     )
     # color rating
     parser.add_argument(
         "-cr",
-        "--color-rating",
+        "--color_rating",
         default=1,
         type=int,
         choices=range(1, 11),  # 1-10
@@ -174,7 +178,7 @@ and a link will be created.",
     )
     parser.add_argument(
         "-ic",
-        "--ignore-color",
+        "--ignore_color",
         action="store_true",
         help="NOTE: Not yet supported -- color ratings are always ignored. \
 Ignore color ratings if set (default).",
@@ -182,7 +186,7 @@ Ignore color ratings if set (default).",
     # source dir
     parser.add_argument(
         "-sd",
-        "--source-dir",
+        "--source_dir",
         default=".",
         metavar="path",
         help="Source directory. Look in this directory for image files. Omit \
@@ -191,7 +195,7 @@ the option or specify '.' to use the working directory.",
     # destination dir
     parser.add_argument(
         "-dd",
-        "--destination-dir",
+        "--destination_dir",
         default="favorites",
         metavar="path",
         help="Destination directory.  Create the links in this directory. \
@@ -210,8 +214,8 @@ destination path.",
     # file types
     parser.add_argument(
         "-f",
-        "--file-types",
-        metavar="file type OR [file type1, file type 2 ...]",
+        "--file_types",
+        metavar="file type OR 'file type1, file type 2 ...'",
         help="Limit processing to specifed file types. Not case sensitive. \
 Valid values are: JPEG, TIFF, GIF, PNG, PSD, INDESIGN, MOV, MP3, MPEG2, MPEG4, \
 AVI, FLV, SWF, ASF, POSTSCRIPT, P2, SONYHDV, AVCHD, UCF, WAV, XDCAM, XDCAMEX.",
@@ -220,7 +224,7 @@ AVI, FLV, SWF, ASF, POSTSCRIPT, P2, SONYHDV, AVCHD, UCF, WAV, XDCAM, XDCAMEX.",
     og_xmp = parser.add_mutually_exclusive_group(required=False)
     og_xmp.add_argument(
         "-x",
-        "--xmp-priority",
+        "--xmp_priority",
         action="store_true",
         help="Give priority to information in an xmp sidecar file if one \
 exists. Without this option, priority is given to the image file over the xmp \
@@ -231,7 +235,7 @@ found.",
     )
     og_xmp.add_argument(
         "-ix",
-        "--ignore-xmp",
+        "--ignore_xmp",
         action="store_true",
         help="Ignore xmp files even if they are present.",
     )
@@ -252,7 +256,7 @@ found.",
     # ignore case if file name otherwise matches the glob or regex pattern
     parser.add_argument(
         "-i",
-        "--ignore-case",
+        "--ignore_case",
         action="store_true",
         help="Ignore case if file name otherwise matches the glob or regex pattern. \
 Ignored if neither -g/--globp or -e/--regexp options are specified.",
@@ -272,7 +276,7 @@ Ignored if neither -g/--globp or -e/--regexp options are specified.",
     )
     # show only errors and warnings
     og_output.add_argument(
-        "--show-errors-and_warnings",
+        "--show_ew",
         action="store_true",
         help="Show only errors and warnings.",
     )
@@ -280,28 +284,30 @@ Ignored if neither -g/--globp or -e/--regexp options are specified.",
     args = parser.parse_args()
 
     # At this point, the arguments will be:
-    # Argument          Type        Default
-    # args.star_rating  int         1
-    # args.ignore_star  bool        False   use star ratings >= 1 by default
-    # args.color_rating int         1
-    # args.ignore_color bool        False   Forced True for now to ignore color rating
-    # args.source_dir   string      .
-    # args.destination_dir string   ./favorites
-    # args.recursive    bool        False
-    # args.fileTypes    string      None    single value, or comma or space or semicolon delimited
-    # args.globp        string      None    (globp | regexp)
-    # args.regexp       string      None
-    # args.ignore-case  bool        False   case sensitive by default
-    # args.verbose      bool        False   Increase messaging (v | q | w)
-    # args.quiet        bool        False   No messaging, not even for errors
-    # args.show-errors-and-warnings bool False
+    # Argument              Type        Default
+    # args.star_rating      int         1
+    # args.ignore_star      bool        False   use star ratings >= 1 by default
+    # args.color_rating     int         1
+    # args.ignore_color     bool        False   Forced True for now to ignore color rating
+    # args.source_dir       string      .
+    # args.destination_dir  string      favorites
+    # args.recursive        bool        False
+    # args.fileTypes        string      None    single value, or comma or space or semicolon delimited
+    # args.xmp_priority     bool        False
+    # args.ignore_xmp       bool        False
+    # args.globp            string      None    (globp | regexp)
+    # args.regexp           string      None
+    # args.ignore_case      bool        False   case sensitive by default
+    # args.verbose          bool        False   Increase messaging (v | q | w)
+    # args.quiet            bool        False   No messaging, not even for errors
+    # args.show_ew          bool        False   Show errors and warnings only
 
     # Force ignore_color to be true for now
     args.ignore_color = True
 
     if args.verbose:
-        # print("\nThe following arguments were parsed:")
-        # print(args)
+        print("\nThe following arguments were parsed:")
+        print(args)
         print(
             "\nNote: ignore_color is forced True at this time. Color ratings \
 are not yet supported."
@@ -310,12 +316,84 @@ are not yet supported."
     # Establish source and destination paths
     src_path = Path(args.source_dir)
     dest_path = Path(args.destination_dir)
-    #if args.verbose:
+    # if args.verbose:
     #    print("The source path is:")
     #    print(src_path.resolve())
     #    print("The destination path is:")
     #    print(dest_path.resolve())
 
+    # Search the source directory for files based on recusive, glob, and regex
+    # options. Since the case-insensitive globp or regexp options will use regex,
+    # simplify the pattern matching by using glob(*)/rgblob(*) and the using
+    # regex to match patterns, converting glopb option to a regex if necessary.
+
+    # Start out with a pattern for all files. Then limit based on options.
+    # Convert globp option to a regex
+    re_pattern = r".*"
+    if args.globp:
+        re_pattern = fnmatch.translate(args.globp)
+    if args.regexp:
+        re_pattern = args.regexp
+    if args.ignore_case:
+        # prepend (?i) to regex pattern to ignore case
+        re_pattern = r"(?i)" + re_pattern
+
+    # At this point, any file name filtering will be done with regex. Glob
+    # patterns were converted to regex above.
+
+    # get all files (including directories), considering recusive search option
+    if args.recursive:
+        contents = src_path.rglob("*")
+    else:
+        contents = src_path.glob("*")
+
+    # Get the files only -- exclude the directories
+    all_files = [file for file in contents if file.is_file()]
+    # if verbose and filtering options are opted, then display the files found
+    # before filtering
+    if args.verbose and args.globp:
+        # extract the path name as a string for display
+        print("\nThe following files were found in the source path before applying the glob pattern:")
+        listPrettyPrint1Col([file.as_posix() for file in all_files])
+    elif args.verbose and args.regexp:
+        # extract the path name as a string for display
+        print("\nThe following files were found in the source path before applying the regex pattern:")
+        listPrettyPrint1Col([file.as_posix() for file in all_files])
+
+    src_files = [] # This will hold the filtered (final) list of path objects
+    # filter out files based on regex filter
+    for path in all_files:
+        try:
+            if re.match(re_pattern, path.as_posix()):
+                src_files.append(path)
+        except re.error as err:
+            if not args.quiet:
+                print("ERROR: Regular Expression Error. Bad escape.")
+                print(err)
+                sys.exit("Exiting.")
+            else:
+                sys.exit(1)
+
+    if args.verbose and args.globp:
+        # extract the path name as a string for display
+        print("\nThe following is a list of source files that have been filtered")
+        print("with the '" + args.globp + "' glob pattern:")
+        if args.ignore_case:
+            print("Note the i-/--ignore_case option is in effect, so file name")
+            print("case will ignored when considerig a match.")
+        listPrettyPrint1Col([file.as_posix() for file in src_files])
+    elif args.verbose and args.regexp:
+        # extract the path name as a string for display
+        print("\nThe following is a list of source files that have been filtered")
+        print("with the '" + args.regexp + "' regex pattern:")
+        if args.ignore_case:
+            print("Note the i-/--ignore_case option is in effect, so file name")
+            print("case will ignored when considerig a match.")
+        listPrettyPrint1Col([file.as_posix() for file in src_files])
+    elif args.verbose: # no glob or regex filtering
+        # extract the path name as a string for display
+        print("\nThe following files were found in the source path:")
+        listPrettyPrint1Col([file.as_posix() for file in src_files])
 
 # Tell python to run main if this program is executed directly (i.e. not imported)
 if __name__ == "__main__":
